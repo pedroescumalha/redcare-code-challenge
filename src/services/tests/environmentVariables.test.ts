cef60e7c9506da845835e71ec5f41f80bfad9162
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-07-03 10:21:45 +0200

    add tests

---
+++ src/services/tests/environmentVariables.test.ts
@@ -0,0 +1,47 @@
+import { beforeEach, describe, it, mock } from "node:test";
+import { loadEnv } from "../env/environmentVariablesService";
+import dotenv from "dotenv";
+import assert from "node:assert";
+import { ValidationError } from "../../common";
+
+describe(loadEnv.name, () => {
+    beforeEach(() => {
+        mock.reset();
+    });
+
+    it("throws if env doesnt fulfill schema", () => {
+        const env = {
+            GITHUB_API_TOKEN: "token",
+            API_URL: "localhost",
+            API_PORT: "invalid_port",
+        };
+
+        mock.method(dotenv, "config", () => {
+            dotenv.populate(process.env as Record<string, string>, env);
+        });
+
+        assert.throws(() => loadEnv(), ValidationError);
+
+        Object.keys(env).forEach((e) => {
+            delete process.env[e];
+        });
+    });
+
+    it("loads correctly", () => {
+        const env = {
+            GITHUB_API_TOKEN: "token",
+            API_URL: "localhost",
+            API_PORT: "8080",
+        };
+
+        mock.method(dotenv, "config", () => {
+            dotenv.populate(process.env as Record<string, string>, env);
+        });
+
+        assert.doesNotThrow(() => loadEnv());
+
+        Object.keys(env).forEach((e) => {
+            delete process.env[e];
+        });
+    });
+});
